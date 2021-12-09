edc4b4aae88b054757bb8226e3f250d0f9821971
LouisDanielA <frixbomberman@gmail.com>
2021-12-10 00:57:30 +0600

    Добавлено приветствие

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ def func():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    print("Hello")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
