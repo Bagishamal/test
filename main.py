fde729d3313c9b6e5b6391b9017e8db57d1f9f1d
LouisDanielA <frixbomberman@gmail.com>
2021-12-10 00:13:25 +0600

    Добавлена переменная

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 a = 100
-
+b = 100
 
 def func():
-    return "Goodbye"
+    return "Goodbye bro"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
